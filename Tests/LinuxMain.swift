9c3985b7369e0515b2bc2e06754674d2c551fed2
Xiaoxing Hu <xiaoxing@iMac.local>
2018-01-04 20:36:19 +1300

    docker, sourcery support

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,27 @@
+// Generated using Sourcery 0.10.1 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
 import XCTest
 @testable import PatternTests
 
+extension PatternTests {
+  static var allTests = [
+    ("testOrgmodePattern", testOrgmodePattern),
+    ("testCapture", testCapture),
+    ("testMergedMachine", testMergedMachine),
+    ("testPatternWithEmojis", testPatternWithEmojis),
+    ("testNestedCapture", testNestedCapture),
+  ]
+}
+
+extension PerfTests {
+  static var allTests = [
+    ("testPerformance", testPerformance),
+  ]
+}
+
+
 XCTMain([
-    testCase(PatternTests.allTests),
+  testCase(PatternTests.allTests),
+  testCase(PerfTests.allTests),
 ])
